1b43872ce65fdc14a3bc9bc583ddf57d63614286
AdityaBhamare <adityabhamare216@gmail.com>
2026-02-01 22:37:23 +0530

    Add solution for problem 282A

---
+++ 282A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+ 
+    int x = 0;
+    string statement;
+ 
+    for (int i = 0; i < n; i++) {
+        cin >> statement;
+        
+        if (statement[1] == '+') {
+            x++;
+        } else {
+            x--;
+        }
+    }
+ 
+    cout << x << endl;
+ 
+    return 0;
+}
