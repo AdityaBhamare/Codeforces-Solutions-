8cfe9b58dab0af6cf652400ce5bc60133a68c027
AdityaBhamare <adityabhamare216@gmail.com>
2026-02-02 21:37:47 +0530

    Add solution for problem 50A

---
+++ 50A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int M, N;
+    
+     if (cin >> M >> N) {
+       
+        int result = (M * N) / 2;
+        
+        cout << result << endl;
+    }
+    
+    return 0;
+}
