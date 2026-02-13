e6497550db4d066e77cf4a01b71bc0088be75684
AdityaBhamare-code <adityabhamare216@gmail.com>
2026-02-13 22:28:44 +0530

    Add solution for problem 734A

---
+++ 734A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int n;
+    string s;
+    
+    if (!(cin >> n)) 
+    return 0;
+    
+    cin >> s;
+    
+    int anton_wins = 0;
+    int danik_wins = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'A') {
+            anton_wins++;
+        } else {
+            danik_wins++;
+        }
+    }
+ 
+    if (anton_wins > danik_wins) {
+        cout << "Anton" << endl;
+    } else if (danik_wins > anton_wins) {
+        cout << "Danik" << endl;
+    } else {
+        cout << "Friendship" << endl;
+    }
+ 
